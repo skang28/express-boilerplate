d0032d47ede1fe3cb07b183a4cc409ab4579ca87
Steven Kang <skang28@gmail.com>
2020-05-08 18:38:17 -0400

    Revert "current working version"
    This reverts commit 144388bcb3e15a2a6e8170053e57fec9c6545cf3.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,4 @@
-require('dotenv').config();
-
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/just-a-calendar',
-  CLIENT_ORIGIN: '*',
-  JWT_SECRET: process.env.JWT_SECRET
-}
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+  }
